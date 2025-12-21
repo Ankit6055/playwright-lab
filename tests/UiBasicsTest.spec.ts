95c54b7b152843a63d6f7a6bdba176dedd00b17d
Ankit Kumar <ankit.kumarrrrr6055@gmail.com>
2025-12-21 17:05:04 +0530

    Add: css locators

--- tests/UiBasicsTest.spec.ts
+++ tests/UiBasicsTest.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("Browser context playwright test", async ({ browser }) => {
+test.skip("Browser context playwright test", async ({ browser }) => {
   const context = browser.newContext(); // Creates a new browser context. It won't share cookies/cache with other browser contexts.
 
   const page = (await context).newPage(); // Creates a new page in the browser context.
@@ -8,10 +8,21 @@ test("Browser context playwright test", async ({ browser }) => {
   (await page).goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
 
-test.only("Page fixture Playwright test", async ({ page }) => {
+test("Page fixture Playwright test", async ({ page }) => {
   await page.goto("https://google.com"); // This is the most common fixture used in a test.
 
   console.log(await page.title()); // Returns the page's title.
 
   await expect(page).toHaveTitle("Google"); // Ensures the page has the given title.
+});
+
+test("Playwright test on a Login page", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  page.locator("#username").fill("rahulshettyacademy");
+  page.locator("[type='password']").fill("learning");
+  await page.locator("#signInBtn").click();
+  console.log(await page.locator("[style*='block']").textContent()); // prints the text content in the pop-up
+
+  await expect(page.locator("[style*='block']")).toContainText("Empty username/password.")
 });
